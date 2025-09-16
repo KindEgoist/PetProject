8906e8d3ecd3039abf241eb5c96e4ec6fb48751f
KindEgoist <djondevil@gmail.com>
2025-09-16 17:02:14 +0700

    добавлено логирование в Payment

--- Payment/src/main/java/com/max/payment/controller/PaymentController.java
+++ Payment/src/main/java/com/max/payment/controller/PaymentController.java
@@ -4,11 +4,12 @@ import com.max.payment.dto.ActionResponse;
 import com.max.payment.dto.PaymentRequest;
 import com.max.payment.exception.AccountNotFoundException;
 import com.max.payment.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
@@ -21,11 +22,18 @@ public class PaymentController {
 
     @PostMapping("/pay")
     public ResponseEntity<ActionResponse> processPayment(@RequestBody PaymentRequest request) {
+
+            log.info("Запрос на оплату: accountId={}, amount={}",
+                    request.getAccountId(), request.getAmount());
+
         try {
             ActionResponse response = paymentService.processPayment(request);
+            log.info("Ответ на запрос оплаты: success={}, message={}",
+                    response.isSuccess(), response.getMessage());
             return ResponseEntity.ok(response);
         }
         catch (AccountNotFoundException e){
+            log.warn("Account не найден: accountId={}", request.getAccountId());
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ActionResponse(false, e.getMessage()));
         }
--- Payment/src/main/java/com/max/payment/service/PaymentServiceImpl.java
+++ Payment/src/main/java/com/max/payment/service/PaymentServiceImpl.java
@@ -7,10 +7,14 @@ import com.max.payment.dto.ReserveRequest;
 import com.max.payment.exception.AccountNotFoundException;
 import com.max.payment.model.Account;
 import com.max.payment.repository.AccountRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
 
+@Slf4j
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
@@ -26,24 +30,50 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     @Transactional
     public ActionResponse processPayment(PaymentRequest request) {
-        Account account = accountRepository.findById(request.getAccountId())
-                .orElseThrow(() -> new AccountNotFoundException(request.getAccountId()));
 
-        if (account.getBalance() < request.getAmount()) {
-            reserveServiceClient.cancelReserve(request);
-            return new ActionResponse(false, "Недостаточно средств");
+        MDC.put("correlationId", UUID.randomUUID().toString());
+
+        log.info("Начала обработка оплаты: accountId={}, amount={}",
+                request.getAccountId(), request.getAmount());
+        try {
+            Account account = accountRepository.findById(request.getAccountId())
+                    .orElseThrow(() -> {
+                        log.warn("Account не найден: accountId={}", request.getAccountId());
+                        return new AccountNotFoundException(request.getAccountId());
+                    });
+
+            if (account.getBalance() < request.getAmount()) {
+                log.warn("Недостаточно средств на счете: accountId={}, balance={}, required={}",
+                        request.getAccountId(), account.getBalance(), request.getAmount());
+                reserveServiceClient.cancelReserve(request);
+                return new ActionResponse(false, "Недостаточно средств");
+            }
+
+            account.setBalance(account.getBalance() - request.getAmount());
+            log.debug("Cписание средств со счета: accountId={}, newBalance={}",
+                    request.getAccountId(), account.getBalance());
+
+            ReserveRequest reserveRequest = new ReserveRequest(request.getProductId(), request.getQuantity());
+
+            ActionResponse commitResponse = reserveServiceClient.commitReserve(reserveRequest);
+
+            if (commitResponse == null || !commitResponse.isSuccess()) {
+                log.warn("Не удалось подтвердить резерв на складе: productId={}, quantity={}",
+                        request.getProductId(), request.getQuantity());
+                return new ActionResponse(false, "Не удалось подтвердить резерв на складе");
+            }
+            log.info("Оплата успешно обработана: accountId={}, amount={}",
+                    request.getAccountId(), request.getAmount());
+
+            return new ActionResponse(true, "Оплата прошла успешно");
+        }
+        catch (Exception e) {
+            log.error("Ошибка при обработке платежа: accountId={}, error={}",
+                    request.getAccountId(), e.getMessage(), e);
+            throw e;
+        } finally {
+            MDC.clear();
         }
 
-        account.setBalance(account.getBalance() - request.getAmount());
-
-        ReserveRequest reserveRequest = new ReserveRequest(request.getProductId(), request.getQuantity());
-
-        ActionResponse commitResponse = reserveServiceClient.commitReserve(reserveRequest);
-
-        if (commitResponse == null || !commitResponse.isSuccess()) {
-            return new ActionResponse(false, "Не удалось подтвердить резерв на складе");
-        }
-
-        return new ActionResponse(true, "Оплата прошла успешно");
     }
 }
